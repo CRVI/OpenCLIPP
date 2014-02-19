0c7f4080fcfea8f4d8aa66bd6450734a26da254b
Antoine W. Campagna <antoine.campagna@crvi.ca>
2014-02-19 16:19:34 -0500

    (Bench): move OpenCV OCL library dependency outside of .vcxproj file
    Closes #1

--- Bench/src/Libraries.h
+++ Bench/src/Libraries.h
@@ -28,9 +28,11 @@
 
 #ifdef HAS_NPP
 #include <npp.h>
+#ifdef _MSC_VER
 #pragma comment ( lib , "nppi" )
 #pragma comment ( lib , "cuda" )
 #pragma comment ( lib , "cudart" )
+#endif _MSC_VER
 #define NPP_AVAILABLE true
 #define NPP_CODE(code) code
 #else
@@ -40,10 +42,12 @@
 
 #ifdef HAS_IPP
 #include <ipp.h>
+#ifdef _MSC_VER
 #pragma comment ( lib , "ippcore" )
 #pragma comment ( lib , "ipps" )
 #pragma comment ( lib , "ippi" )
 #pragma comment ( lib , "ippcv" )
+#endif _MSC_VER
 #define IPP_AVAILABLE true
 #define IPP_CODE(code) code
 #else
@@ -61,6 +65,10 @@
 #ifdef HAS_CV
 #include <opencv2/opencv.hpp>
 #include <opencv2/ocl/ocl.hpp>
+#ifdef _MSC_VER
+#pragma comment ( lib , "opencv_core246" )
+#pragma comment ( lib , "opencv_ocl246" )
+#endif _MSC_VER
 using namespace cv;
 using namespace ocl;
 #define CV_CODE(code) code
